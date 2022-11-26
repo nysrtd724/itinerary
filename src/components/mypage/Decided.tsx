8aad302cc49e5bc33cf95c2631d11e75709350a5
yuri <yuri090308@icloud.com>
2022-11-27 00:27:17 +0900

    しおり詳細ページid取得

--- src/components/mypage/Decided.tsx
+++ src/components/mypage/Decided.tsx
@@ -1,11 +1,28 @@
-import React from "react";
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { db } from "../firebase";
 
 function Decided() {
+  const { id } = useParams();
+
+  useEffect(() => {
+    if (id) {
+      const itineraryDate = doc(db, "itinerary", id);
+      getDoc(itineraryDate).then((documentSnapshot) => {
+        console.log(documentSnapshot.data());
+      });
+      //getDocs(itineraryDate).then((querySnapshot) => {
+      //  setItinerary(querySnapshot.docs);
+      //});
+    }
+  }, []);
+
   return (
     <div>
       <div>
         <h2>大阪旅行</h2>
-        <h3>2022年11月1日</h3>
+        {/*<h3>2022年11月1日</h3>
         <span>〜</span>
         <h3>2022年11月2日</h3>
       </div>
@@ -16,7 +33,7 @@ function Decided() {
         <h3>2022年11月1日</h3>
         <p>8:00</p>
         <h3>東京駅</h3>
-        <p>各自切符を持ってくる</p>
+        <p>各自切符を持ってくる</p>*/}
       </div>
     </div>
   );
